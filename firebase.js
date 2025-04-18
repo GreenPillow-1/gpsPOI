ab40a12183242c3f34460b60ad2e288f8040bb5d
John <g15451452@gmail.com>
2025-04-18 22:16:25 +1000

    Updated Firebase config for security

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCxIQOZaIaMgfMOnbOSe_sGeHLAuBt8_1w",
-  authDomain: "poi-finder-e2a61.firebaseapp.com",
-  projectId: "poi-finder-e2a61",
-  storageBucket: "poi-finder-e2a61.appspot.com",
-  messagingSenderId: "1088168623325",
-  appId: "1:1088168623325:web:3dce6f5417ce36ef6daf34",
+  apiKey: "insert",
+  authDomain: "insert",
+  projectId: "insert",
+  storageBucket: "insert",
+  messagingSenderId: "insert",
+  appId: "insert",
 };
 
 const app = initializeApp(firebaseConfig);
